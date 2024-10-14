ca0bb4b9d7bb36ed8d7673e58c37645306dcb032
Gizlim <gizelle9lim@gmail.com>
2024-10-14 12:14:22 +0800

    added object detection and coordinate retrieval for gazebo

--- build/navigation/CMakeFiles/navigation_generate_messages_cpp.dir/cmake_clean.cmake
+++ build/navigation/CMakeFiles/navigation_generate_messages_cpp.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/include/navigation/ConvertPixelToMap.h"
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/include/navigation/ConvertPixelWaypointsToMap.h"
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/include/navigation/GetAmclPose.h"
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/include/navigation/GetPixelPose.h"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/include/navigation/ConvertPixelToMap.h"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/include/navigation/ConvertPixelWaypointsToMap.h"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/include/navigation/GetAmclPose.h"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/include/navigation/GetPixelPose.h"
   "CMakeFiles/navigation_generate_messages_cpp"
 )
 
--- build/navigation/CMakeFiles/navigation_generate_messages_nodejs.dir/cmake_clean.cmake
+++ build/navigation/CMakeFiles/navigation_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/share/gennodejs/ros/navigation/srv/ConvertPixelToMap.js"
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/share/gennodejs/ros/navigation/srv/ConvertPixelWaypointsToMap.js"
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/share/gennodejs/ros/navigation/srv/GetAmclPose.js"
-  "/home/keith/Documents/Capstone/Robot_Kepstone/Test_Robot/devel/share/gennodejs/ros/navigation/srv/GetPixelPose.js"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/share/gennodejs/ros/navigation/srv/ConvertPixelToMap.js"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/share/gennodejs/ros/navigation/srv/ConvertPixelWaypointsToMap.js"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/share/gennodejs/ros/navigation/srv/GetAmclPose.js"
+  "/home/gl/Documents/CAPSTONE/Kepstone_Experimental_Robot/devel/share/gennodejs/ros/navigation/srv/GetPixelPose.js"
   "CMakeFiles/navigation_generate_messages_nodejs"
 )
 
