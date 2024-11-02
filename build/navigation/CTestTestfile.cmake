6102af9bed4aed2092213682718f5dd58358b842
kwokkeith <keithkwok99@gmail.com>
2024-11-02 15:57:48 +0800

    Deleted build and devel folder

--- build/navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/keith/Documents/Capstone/Kepstone_Experimental_Robot/src/navigation
-# Build directory: /home/keith/Documents/Capstone/Kepstone_Experimental_Robot/build/navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/navigation/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/keith/Documents/Capstone/Kepstone_Experimental_Robot/devel/include;/home/keith/Documents/Capstone/Kepstone_Experimental_Robot/src/navigation/include".split(';') if "/home/keith/Documents/Capstone/Kepstone_Experimental_Robot/devel/include;/home/keith/Documents/Capstone/Kepstone_Experimental_Robot/src/navigation/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "std_msgs;geometry_msgs;message_runtime;bumperbot_utils;litter_destruction;tf".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "navigation"
-PROJECT_SPACE_DIR = "/home/keith/Documents/Capstone/Kepstone_Experimental_Robot/devel"
-PROJECT_VERSION = "0.0.0"
