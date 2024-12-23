187720b5cd4dd3b10c649979d68ac78b3ca19ab2
junonn <junonn2002@gmail.com>
2024-12-24 00:05:07 +0800

    update db_publisher

--- src/coverage_planner/src/db_publisher_node.cpp
+++ src/coverage_planner/src/db_publisher_node.cpp
@@ -7,53 +7,53 @@
 std::string db_path;
 
 sqlite3 *db;
+bool polygonStored = false;
+bool pathStored = false;
+bool mapNameStored = false;
 
 void canvasMessengerCallback(const std_msgs::String::ConstPtr& msg) {
     ROS_INFO("I heard: [%s]", msg->data.c_str());
-
-    // Insert data into the database
-    std::stringstream ss;
-    ss << "INSERT INTO messages (topic, content) VALUES ('/canvas_messenger', '" << msg->data << "');";
-    std::string sql = ss.str();
-
-    char *errMsg = 0;
-    int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg);
-    if (rc != SQLITE_OK) {
-        ROS_ERROR("SQL error: %s", errMsg);
-        sqlite3_free(errMsg);
-    } else {
-        ROS_INFO("Data inserted successfully");
+    if (!polygonStored){
+        std::stringstream ss;
+        ss << "INSERT INTO messages (topic, content) VALUES ('polygon', '" << msg->data << "');";
+        char* errMsg = 0;
+        int rc = sqlite3_exec(db, ss.str().c_str(), 0, 0, &errMsg);
+        if (rc != SQLITE_OK) { ROS_ERROR("SQL error: %s", errMsg); sqlite3_free(errMsg); }
+        else { ROS_INFO("Polygon inserted"); }
+        polygonStored = true;
+    } else if (!pathStored) {
+        // Insert path data
+        std::stringstream ss;
+        ss << "INSERT INTO messages (topic, content) VALUES ('path', '" << msg->data << "');";
+        char* errMsg = 0;
+        int rc = sqlite3_exec(db, ss.str().c_str(), 0, 0, &errMsg);
+        if (rc != SQLITE_OK) { ROS_ERROR("SQL error: %s", errMsg); sqlite3_free(errMsg); }
+        else { ROS_INFO("Path inserted"); }
+        pathStored = true;
     }
 }
 
 void mapNameCallback(const std_msgs::String::ConstPtr& msg) {
-    ROS_INFO("I heard: [%s]", msg->data.c_str());
-
-    // Insert data into the database
-    std::stringstream ss;
-    ss << "INSERT INTO messages (topic, content) VALUES ('/new_map_name', '" << msg->data << "');";
-    std::string sql = ss.str();
-
-    char *errMsg = 0;
-    int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg);
-    if (rc != SQLITE_OK) {
-        ROS_ERROR("SQL error: %s", errMsg);
-        sqlite3_free(errMsg);
-    } else {
-        ROS_INFO("Data inserted successfully");
+    // ROS_INFO("I heard: [%s]", msg->data.c_str());
+    if (!mapNameStored){
+        std::stringstream ss;
+        ss << "INSERT INTO messages (topic, content) VALUES ('map_name', '" << msg->data << "');";
+        char *errMsg = 0;
+        int rc = sqlite3_exec(db, ss.str().c_str(), 0, 0, &errMsg);
+        if (rc != SQLITE_OK) { ROS_ERROR("SQL error: %s", errMsg); sqlite3_free(errMsg); }
+        else { ROS_INFO("Map name inserted"); }
+        mapNameStored = true;
     }
 }
 
 int main(int argc, char **argv) {
-    std::string GUI_package_path = ros::package::getPath("bumperbot_graphical_interface");
     ros::init(argc, argv, "subscriber_node");
     ros::NodeHandle nh;
 
+    std::string GUI_package_path = ros::package::getPath("bumperbot_graphical_interface");
     db_path= GUI_package_path + "/db/coverage_zones.db";
-
     // Open the SQLite database
-    int rc = sqlite3_open(db_path.c_str(), &db);
-    if (rc) {
+    if (sqlite3_open(db_path.c_str(), &db)) {
         ROS_ERROR("Can't open database: %s", sqlite3_errmsg(db));
         return 1;
     } else {
@@ -66,8 +66,7 @@ int main(int argc, char **argv) {
                                    "topic TEXT NOT NULL," \
                                    "content TEXT NOT NULL);";
     char *errMsg = 0;
-    rc = sqlite3_exec(db, sql_create_table, 0, 0, &errMsg);
-    if (rc != SQLITE_OK) {
+    if (sqlite3_exec(db, sql_create_table, 0, 0, &errMsg) != SQLITE_OK) {
         ROS_ERROR("SQL error: %s", errMsg);
         sqlite3_free(errMsg);
         return 1;
@@ -77,7 +76,6 @@ int main(int argc, char **argv) {
 
     ros::Subscriber point_sub = nh.subscribe("/canvas_messenger", 1000, canvasMessengerCallback);
     ros::Subscriber map_name_sub = nh.subscribe("/new_map_name", 1000, mapNameCallback);
-
     ros::spin();
 
     // Close the SQLite database
