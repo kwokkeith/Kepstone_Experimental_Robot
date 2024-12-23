a99fd71456981cc8f3ce91827a691f9a5b5c5fc0
junonn <junonn2002@gmail.com>
2024-12-24 01:09:17 +0800

    added id, name, poly, cleaning columns

--- src/coverage_planner/src/db_publisher_node.cpp
+++ src/coverage_planner/src/db_publisher_node.cpp
@@ -7,43 +7,53 @@
 std::string db_path;
 
 sqlite3 *db;
-bool polygonStored = false;
-bool pathStored = false;
+bool polygonBoundingCoordinatesStored = false;
+bool cleaningPathCoordinatesStored = false;
 bool mapNameStored = false;
+
+std::string map_name;
+std::string polygonBounding_coordinates;
+std::string cleaning_path_coordinates;
+//TODO: Confirm with keith and Gizelle if the name cleaning_path_coordinates should be changed to waypoints?
+//TODO: Add images into db
+void insertData(){
+    if (mapNameStored && polygonBoundingCoordinatesStored && cleaningPathCoordinatesStored){
+        std::stringstream ss;
+        ss << "INSERT INTO messages (map_name, polygonBounding_coordinates, cleaning_path_coordinates) VALUES ('"
+              << map_name << "', '" << polygonBounding_coordinates << "', '" << cleaning_path_coordinates << "');";
+        char* errMsg = nullptr;
+        int rc = sqlite3_exec(db, ss.str().c_str(), 0, 0, &errMsg);
+        if (rc != SQLITE_OK) { ROS_ERROR("SQL error: %s", errMsg); sqlite3_free(errMsg); }
+        else { ROS_INFO("Data inserted successfully"); }
+
+        mapNameStored = false;
+        polygonBoundingCoordinatesStored = false;
+        cleaningPathCoordinatesStored = false;
+        map_name.clear();
+        polygonBounding_coordinates.clear();
+        cleaning_path_coordinates.clear();
+    }
+}
 
 void canvasMessengerCallback(const std_msgs::String::ConstPtr& msg) {
     ROS_INFO("I heard: [%s]", msg->data.c_str());
-    if (!polygonStored){
-        std::stringstream ss;
-        ss << "INSERT INTO messages (topic, content) VALUES ('polygon', '" << msg->data << "');";
-        char* errMsg = 0;
-        int rc = sqlite3_exec(db, ss.str().c_str(), 0, 0, &errMsg);
-        if (rc != SQLITE_OK) { ROS_ERROR("SQL error: %s", errMsg); sqlite3_free(errMsg); }
-        else { ROS_INFO("Polygon inserted"); }
-        polygonStored = true;
-    } else if (!pathStored) {
-        // Insert path data
-        std::stringstream ss;
-        ss << "INSERT INTO messages (topic, content) VALUES ('path', '" << msg->data << "');";
-        char* errMsg = 0;
-        int rc = sqlite3_exec(db, ss.str().c_str(), 0, 0, &errMsg);
-        if (rc != SQLITE_OK) { ROS_ERROR("SQL error: %s", errMsg); sqlite3_free(errMsg); }
-        else { ROS_INFO("Path inserted"); }
-        pathStored = true;
+    if (!polygonBoundingCoordinatesStored){
+        polygonBounding_coordinates = msg->data;
+        polygonBoundingCoordinatesStored = true;
+    } else if (!cleaningPathCoordinatesStored && msg->data != polygonBounding_coordinates) {
+        cleaning_path_coordinates = msg->data;
+        cleaningPathCoordinatesStored = true;
     }
+    insertData();
 }
 
 void mapNameCallback(const std_msgs::String::ConstPtr& msg) {
     // ROS_INFO("I heard: [%s]", msg->data.c_str());
     if (!mapNameStored){
-        std::stringstream ss;
-        ss << "INSERT INTO messages (topic, content) VALUES ('map_name', '" << msg->data << "');";
-        char *errMsg = 0;
-        int rc = sqlite3_exec(db, ss.str().c_str(), 0, 0, &errMsg);
-        if (rc != SQLITE_OK) { ROS_ERROR("SQL error: %s", errMsg); sqlite3_free(errMsg); }
-        else { ROS_INFO("Map name inserted"); }
+        map_name = msg->data;
         mapNameStored = true;
     }
+    insertData();
 }
 
 int main(int argc, char **argv) {
@@ -63,8 +73,9 @@ int main(int argc, char **argv) {
     // Create the table if it doesn't exist
     const char *sql_create_table = "CREATE TABLE IF NOT EXISTS messages (" \
                                    "id INTEGER PRIMARY KEY AUTOINCREMENT," \
-                                   "topic TEXT NOT NULL," \
-                                   "content TEXT NOT NULL);";
+                                   "map_name TEXT NOT NULL," \
+                                   "polygonBounding_coordinates TEXT NOT NULL," \
+                                   "cleaning_path_coordinates TEXT NOT NULL);";
     char *errMsg = 0;
     if (sqlite3_exec(db, sql_create_table, 0, 0, &errMsg) != SQLITE_OK) {
         ROS_ERROR("SQL error: %s", errMsg);
@@ -77,6 +88,7 @@ int main(int argc, char **argv) {
     ros::Subscriber point_sub = nh.subscribe("/canvas_messenger", 1000, canvasMessengerCallback);
     ros::Subscriber map_name_sub = nh.subscribe("/new_map_name", 1000, mapNameCallback);
     ros::spin();
+    
 
     // Close the SQLite database
     sqlite3_close(db);
