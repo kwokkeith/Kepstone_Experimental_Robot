1bccced03ce5aa84499b7d1d0acbe68d64cc45b1
gizelle <gizelle9lim@gmail.com>
2025-02-03 20:34:40 +0800

    connected realsense to gazebo

---
+++ src/bumperbot_detection/src/cv_api_get_coordinates.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+import rospy
+import cv2
+import numpy as np
+import math
+from decimal import Decimal
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import message_filters  # To synchronize color and depth frames
+from geometry_msgs.msg import PointStamped
+from ultralytics import YOLO
+import os
+from sensor_msgs.msg import CameraInfo
+
+
+# This class is used to detect object (ball) and publish to camera_frame/detected_object_coordinates
+# Sub:
+#   /camera/color/image_raw
+# Pub:
+#   /camera_frame_detected_object_coordinates
+# Node Param:  
+#   camera_frame
+
+class BallDetector:
+    def __init__(self):
+        ## Get global paramaters for topics/services
+        detected_object_coordinates_topic_pub_ = rospy.get_param('/litter_detection/topics/detected_object_coordinates_camera')
+        # camera_color_image_raw_client = rospy.get_param('/camera/topics/color_image_raw')
+        # camera_depth_image_raw_client = rospy.get_param('/camera/topics/depth_image_raw')
+
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        model_path = os.path.join(current_dir, "../models/yolov8.pt")
+        self.model = YOLO(model_path)
+        
+        self.bridge_object = CvBridge()
+        self.intrinsics = None  # Placeholder for intrinsics
+        self.theta = rospy.get_param('/litter_detection/camera_theta')
+
+        # Get the camera frame from the ROS parameter server (default to 'dpcamera_link')
+        self.camera_frame = rospy.get_param('~camera_frame', 'dpcamera_link')
+
+        self.coord_publisher = rospy.Publisher(detected_object_coordinates_topic_pub_, PointStamped, queue_size=10)
+
+        # Subscribe to color and depth topics
+        self.color_sub = message_filters.Subscriber('/camera/color/image_raw', Image)
+        self.depth_sub = message_filters.Subscriber('/camera/depth/image_rect_raw', Image)
+
+        # Subscribe to camera info topic to get intrinsics
+        rospy.Subscriber("/camera/depth/camera_info", CameraInfo, self.camera_info_callback)
+
+        # Synchronize the color and depth messages
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.color_sub, self.depth_sub], 10, 0.5)
+        self.ts.registerCallback(self.image_callback)
+
+        # Manually added based on gazebo depth camera intrinsics
+
+        # Frame rate variables
+        self.new_frame_time = 0
+        self.prev_frame_time = 0
+
+        self.last_image = None
+
+    # Extract camera intrinsics from CameraInfo message.
+    def camera_info_callback(self, msg):
+        self.intrinsics = {
+            'fx': msg.K[0],
+            'fy': msg.K[4],
+            'cx': msg.K[2],
+            'cy': msg.K[5]
+        }
+        rospy.loginfo(f"Intrinsics: {self.intrinsics}")
+
+
+    def image_callback(self, color_msg, depth_msg):
+        try:
+            print("Received new frame!")  # Debugging
+            
+            # Convert ROS image messages to OpenCV images
+            color_image = self.bridge_object.imgmsg_to_cv2(color_msg, "bgr8")
+            depth_image = self.bridge_object.imgmsg_to_cv2(depth_msg, "16UC1")  # Use 16-bit encoding
+
+            # depth_array = np.array(depth_image, dtype=np.float32)
+
+            # # Debugging: Print min/max depth
+            # print(f"Min Depth: {np.min(depth_array)}, Max Depth: {np.max(depth_array)}")
+
+            # # Normalize depth image for visualization
+            # if np.max(depth_array) > 0:
+            #     depth_norm = cv2.normalize(depth_array, None, 0, 255, cv2.NORM_MINMAX)
+            #     depth_norm = np.uint8(depth_norm * (255.0 / np.max(depth_norm)))  # Ensure scaling
+            # else:
+            #     depth_norm = np.zeros_like(depth_array, dtype=np.uint8)  # Avoid division by zero
+
+            # depth_colormap = cv2.applyColorMap(depth_norm, cv2.COLORMAP_JET)  # Apply colormap
+
+            # # Resize depth map to match color image dimensions (optional)
+            # depth_colormap_resized = cv2.resize(depth_colormap, (color_image.shape[1], color_image.shape[0]))
+
+
+        except CvBridgeError as e:
+            print(e)
+            return
+
+        # Perform object detection on color image
+        image, center_coordinates_array = self.detect_image(color_image)
+
+        # Calculate real-world coordinates using depth data
+        for i in range(len(center_coordinates_array)):
+            x = int(center_coordinates_array[i][0])
+            y = int(center_coordinates_array[i][1])
+
+
+            ### TODO: DEPTH NOT WORKING PROPERLY
+            
+            depth_value = depth_image[y, x]  # Get the depth value at (x, y)
+            if not np.isfinite(depth_value):  # Skip if depth value is invalid (NaN or inf)
+                continue
+            # dist = depth_value * 1000  # Convert depth from meters to mm
+            dist = depth_value           # Depth in mm
+
+            # Calculate X, Y, Z real-world coordinates
+            Xtemp = dist * (x - self.intrinsics['cx']) / self.intrinsics['fx']
+            Ytemp = dist * (y - self.intrinsics['cy']) / self.intrinsics['fy']
+            Ztemp = dist
+
+            Xtarget = Xtemp  # Adjust for RGB camera module offset
+            Ytarget = -(Ztemp * math.sin(self.theta) + Ytemp * math.cos(self.theta))
+            Ztarget = Ztemp * math.cos(self.theta) - Ytemp * math.sin(self.theta)
+
+            # Create a PointStamped message
+            point_msg = PointStamped()
+            point_msg.header.stamp = rospy.Time.now()     # Add a timestamp
+            point_msg.header.frame_id = self.camera_frame # Indicate the frame of reference
+            
+            # Point coordinates are in robot coordinate system (point) as opposed to the 
+            # optical coordinate system of camera (Xtarget, Ytarget, Ztarget)
+            point_msg.point.y = -Xtarget
+            point_msg.point.z = Ytarget
+            point_msg.point.x = Ztarget
+
+            self.coord_publisher.publish(point_msg)
+
+            # Display coordinates on image with up to 3 decimal places
+            target_coordinates = f"({point_msg.point.x:.3f}, {point_msg.point.y:.3f}, {point_msg.point.z:.3f})"
+
+            # Add the text with the coordinates to the image
+            cv2.putText(image, target_coordinates, (x - 160, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+
+        # Calculate and display FPS
+        self.new_frame_time = rospy.Time.now().to_sec()
+
+        # Check for division by 0
+        if (self.new_frame_time - self.prev_frame_time) > 0:
+            fps = 1 / (self.new_frame_time - self.prev_frame_time)
+        else:
+            fps = 0  # Default value to avoid division by zero
+        fps_text = str(int(fps))
+        cv2.putText(image, fps_text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3, cv2.LINE_AA)
+        self.prev_frame_time = self.new_frame_time
+
+        # Show the image with detections
+        # cv2.imshow('result', image)
+        # return Xtarget, Ytarget, Ztarget
+        self.last_image = image
+
+    def detect_image(self, color_image):
+
+        center_coordinates_array = []
+        results = self.model(color_image)
+
+        for result in results:
+            boxes = result.boxes
+            for box in boxes:
+                x_min, y_min, x_max, y_max = box.xyxy[0].cpu().numpy().astype(int)
+                confidence = box.conf[0].cpu().numpy()
+                confidence_text = f"{confidence:.2f}"
+
+                if confidence < 0.2:  
+                    continue
+
+                # Get the center point of the bounding box
+                x_center = (x_min + x_max) / 2
+                y_center = (y_min + y_max) / 2
+                center_coordinates_array.append([x_center, y_center])
+
+                cv2.rectangle(color_image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255, 0, 0), 2)
+                cv2.putText(color_image, (f"CONFIDENCE:{confidence_text}"), (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                
+
+        # cv2.imshow("original", color_image)
+        return color_image, center_coordinates_array  # Example: returning a single point in the center of the image
+
+
+if __name__ == "__main__":
+    rospy.init_node('ball_detector', anonymous=True)
+    detector = BallDetector()
+    # rate = rospy.Rate(10)
+    # rospy.spin()
+
+    while not rospy.is_shutdown():
+        if detector.last_image is not None:
+            # Display the last processed image
+            cv2.imshow('Detected Objects', detector.last_image)
+            cv2.waitKey(1)  # Add a delay to refresh the display window
+        rospy.sleep(0.01)
+
+    cv2.destroyAllWindows()
