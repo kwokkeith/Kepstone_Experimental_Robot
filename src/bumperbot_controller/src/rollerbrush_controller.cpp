413ca133a29040f547122d91606a8869b2fb69a5
gizlim <gizelle9lim@gmail.com>
2025-02-07 18:31:19 +0800

    added rollerbrush power node

---
+++ src/bumperbot_controller/src/rollerbrush_controller.cpp
@@ -0,0 +1,79 @@
+#include "bumperbot_controller/rollerbrush_controller.h"
+
+RollerBrushController::RollerBrushController(ros::NodeHandle& nh) : 
+    nh_(nh), 
+    current_power_(0.0)
+{
+    // GET global parameters for services/topics
+    // Service Servers
+    std::string set_rollerbrush_power_srv_svr_, get_rollerbrush_power_srv_svr_;
+    nh_.getParam("/rollerbrush_controller/services/get_rollerbrush_power", get_rollerbrush_power_srv_svr_);
+    nh_.getParam("/rollerbrush_controller/services/set_rollerbrush_power", set_rollerbrush_power_srv_svr_);
+    // Topic Publishers
+    std::string rollerbrush_power_topic_pub_;
+    nh_.getParam("/rollerbrush_controller/topics/rollerbrush_power", rollerbrush_power_topic_pub_);
+
+    // Initialize Service Servers
+    set_rollerbrush_service_ = nh_.advertiseService(set_rollerbrush_power_srv_svr_, &RollerBrushController::setRollerBrushPower, this);
+    get_rollerbrush_service_ = nh_.advertiseService(get_rollerbrush_power_srv_svr_, &RollerBrushController::getRollerBrushPower, this);
+    // Initialize Topic Publishers
+    rollerbrush_pub_ = nh_.advertise<std_msgs::Float32>(rollerbrush_power_topic_pub_, 10);
+
+
+    // Publish default rollerbrush power (0.0)
+    std_msgs::Float32 msg;
+    msg.data = current_power_;
+    rollerbrush_pub_.publish(msg);
+
+    ROS_INFO("RollerBrush Controller Node Initialized.");
+}
+
+bool RollerBrushController::setRollerBrushPower(bumperbot_controller::SetRollerBrushPower::Request& req, 
+                                      bumperbot_controller::SetRollerBrushPower::Response& res)
+{
+    try 
+    {
+        // Validate input range
+        if (req.power < 0.0 || req.power > 1.0)
+        {
+            res.success = false;
+            res.message = "Invalid power level! Please set a value between 0.0 and 1.0 (inclusive).";
+            ROS_WARN_STREAM(res.message);
+            return false;
+        }
+
+        // Set new power level
+        current_power_ = req.power;
+        std_msgs::Float32 msg;
+        msg.data = current_power_;
+        rollerbrush_pub_.publish(msg);
+
+        res.success = true;
+        res.message = "RollerBrush power set successfully to " + std::to_string(current_power_);
+        ROS_INFO_STREAM(res.message);
+
+        return true;
+    }
+    catch (const std::exception& e) 
+    {
+        ROS_ERROR_STREAM("Exception in setRollerBrushPower: " << e.what());
+        res.success = false;
+        res.message = "Internal server error.";
+        return false;
+    }
+    catch (...)
+    {
+        ROS_ERROR("Unknown exception in setRollerBrushPower");
+        res.success = false;
+        res.message = "Unknown internal error.";
+        return false;
+    }
+}
+
+bool RollerBrushController::getRollerBrushPower(bumperbot_controller::GetRollerBrushPower::Request& req, 
+                                      bumperbot_controller::GetRollerBrushPower::Response& res)
+{
+    res.power = current_power_;
+    ROS_INFO_STREAM("Returning current rollerbrush power: " << current_power_);
+    return true;
+}
